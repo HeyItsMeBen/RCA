b1f27f35c3c9a25367f07f57044b5d7f6560c246
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2026-07-07 17:51:47 -0700

    dashboard

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm/DashboardTester.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.Arm;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp(name="Dashboard Servo Test", group="Debug")
+public class DashboardTester extends LinearOpMode {
+
+    public static double ELBOW_SERVO_POSITION = 0.0;
+
+    @Override
+    public void runOpMode() {
+
+        Servo servo = hardwareMap.get(Servo.class, "ElbowHinge");
+
+        telemetry = new MultipleTelemetry(
+                telemetry,
+                FtcDashboard.getInstance().getTelemetry()
+        );
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            servo.setPosition(ELBOW_SERVO_POSITION);
+
+            telemetry.addData("Servo Position", ELBOW_SERVO_POSITION);
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
